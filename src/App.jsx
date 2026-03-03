275eb4be51870f9b77c1d6fcfee39b44fba341ce
kazimirwang <kzbwang@gmail.com>
2026-03-02 20:49:56 -0800

    added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
 import './App.css';
-import Header from './components/Header';
-import Food from './components/Food';
-import Footer from './components/Footer';
-import AboutMe from './components/AboutMe';
-import DreamJob from './components/DreamJob';
-import Hobby from './components/Hobby';
+import Nav from './components/Nav'
 
 function App() {
   return (
     <>
-      <Hobby />
-      {/* <Header />
-      <main>
-        <h2>Welcome to my website!</h2>
-        <h1
-          className="text-4xl font-bold text-blue-600
-	bg-yellow-100 p-8 rounded-xl"
-        >
-          Tailwind is working!
-        </h1>
-
-        <Food />
-        <br />
-        <AboutMe />
-        <br />
-        <DreamJob />
-        <br />
-        <Hobby />
-      </main>
-      <Footer /> */}
+      <Nav/>
     </>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+    return(
+        <nav className="fixed bottom-0 columns-3 h-20 w-full bg-emerald-300 text-center">
+            <a href="#"><img src="#" alt="1" /></a>
+            <a href="#"><img src="#" alt="2" /></a>
+            <a href="#"><img src="#" alt="3" /></a>
+        </nav>
+    );
+}
+
+export default Nav;
