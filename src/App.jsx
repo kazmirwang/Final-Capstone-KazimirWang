a077a68eae63d0e66f7739b5a4182b725b6f743d
kazmirwang <133053799+kazmirwang@users.noreply.github.com>
2026-04-27 11:51:20 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
     <>
       <div className="fixed h-full w-full -z-10 bg-amber-50"></div>
       <div className="fixed h-1 w-full bg-amber-50"></div>
-      <BrowserRouter basename="/Final-Capstone-KazimirWang/">
+      <BrowserRouter basename="/Final-Capstone-KazimirWang">
         <nav className="fixed bottom-0 h-20 w-full columns-3 bg-emerald-300 text-center">
           <Link to="/tracker" className="hover:bg-emerald-500 h-full block">
             <img
